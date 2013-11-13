089d863579155dfc3fc57d64383add3c19e56ca1
alexflores <axel_f_g@hotmail.com>
2013-11-13 19:39:09 -0400

    Bienvenida al ahorcado

---
+++ src/main/java/AhorcadoTDD/Bienvenida.java
@@ -0,0 +1,12 @@
+package AhorcadoTDD;
+
+public class Bienvenida {
+
+   private String _BIENVENIDA="Bienvenido al juego del Ahorcado";	
+	
+   public void bienvenida()
+   {
+	  System.out.println(_BIENVENIDA); 
+   }
+	
+}
---
+++ src/main/java/AhorcadoTDD/Principal.java
@@ -0,0 +1,10 @@
+package AhorcadoTDD;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Bienvenida b = new Bienvenida();
+		b.bienvenida();
+	}
+
+}
